f7ef41aeef8c4bd86e6cd2a5502bf909de685bc8
dadhikary2560 <d.adhikary2560@gmail.com>
2026-02-10 13:47:22 +0530

    reverse a string using split method of String class

---
+++ src/stringPrograms/ReverseStringUsingStringMethodPART2.java
@@ -0,0 +1,12 @@
+package stringPrograms;
+
+public class ReverseStringUsingStringMethodPART2 {
+public static void main(String[] args) {
+	String s=new String("Debabrat");
+	String arr[]=s.split("");
+	for(int i=arr.length-1;i>=0;i--)
+	{
+		System.out.print(arr[i]);
+	}
+}
+}
