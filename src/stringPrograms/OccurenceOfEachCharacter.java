ff75d2df28f8a99daf431dcb853b9719ac714bb0
dadhikary2560 <d.adhikary2560@gmail.com>
2026-02-11 17:25:10 +0530

    occurence of each letter in a string

---
+++ src/stringPrograms/OccurenceOfEachCharacter.java
@@ -0,0 +1,67 @@
+package stringPrograms;
+
+public class OccurenceOfEachCharacter {
+	//IIIINNDDDIIIAAAAAA
+	//I4N2D3I3A6
+	public void india() {
+		String s="IIIINNDDDIIIAAAAAA";
+		for(int i=0;i<s.length();i++)
+		{
+
+			boolean isFound=false;
+			for(int k=0;k<i;k++)
+			{
+				if(s.charAt(i)==s.charAt(k))
+				{
+					isFound=true;
+					break;
+				}
+			}
+			if(isFound)
+				continue;
+			int count=1;
+			for(int j=i+1;j<s.length();j++)
+			{
+				for(int n=j+1;n<s.length();n++)
+				{
+					if(s.charAt(i)==s.charAt(j))
+						count++;
+				}
+			}
+			if(count>0)
+				System.out.print(s.charAt(i)+""+count);
+		}
+
+	}
+	
+public static void main(String[] args) {
+	OccurenceOfEachCharacter obj=new OccurenceOfEachCharacter();
+	obj.india();
+	System.out.println();
+	
+	
+	String s=new String("Hello World");
+	for(int i=0;i<s.length();i++)
+	{
+		boolean isFound=false;
+		for(int k=0;k<i;k++)
+		{
+			if(s.charAt(i)==s.charAt(k))
+			{
+				isFound=true;
+				break;
+			}
+		}
+		if(isFound)
+			continue;
+		int count=1;
+		for(int j=i+1;j<s.length();j++)
+		{
+			if(s.charAt(i)==s.charAt(j))
+				count++;
+		}
+		if(count>0 && !Character.isWhitespace(s.charAt(i)))
+			System.out.println("Occurence of letter "+s.charAt(i)+" is: "+count);
+	}
+}
+}
