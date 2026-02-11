d090392400ba2e5fb040ec35e55775e9294bd7fb
dadhikary2560 <d.adhikary2560@gmail.com>
2026-02-11 17:25:51 +0530

    find vowels from a string and print them

---
+++ src/stringPrograms/VowelsFromString.java
@@ -0,0 +1,18 @@
+package stringPrograms;
+
+public class VowelsFromString {
+public static void main(String[] args) {
+	String s="HeEllo WoOrldA";
+	s=s.toLowerCase();
+	
+	for(int i=0;i<s.length();i++)
+	{
+		if(s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || 
+				s.charAt(i)=='o' || s.charAt(i)=='u')
+		{
+			System.out.print(s.charAt(i)+" ");
+		}
+			
+	}
+}
+}
