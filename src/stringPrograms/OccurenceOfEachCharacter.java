e4e1b293baa2b14001d68402325d1732d7c41c24
dadhikary2560 <d.adhikary2560@gmail.com>
2026-02-12 14:21:55 +0530

    updated with I4N2D2I4A6 program

--- src/stringPrograms/OccurenceOfEachCharacter.java
+++ src/stringPrograms/OccurenceOfEachCharacter.java
@@ -5,31 +5,22 @@ public class OccurenceOfEachCharacter {
 	//I4N2D3I3A6
 	public void india() {
 		String s="IIIINNDDDIIIAAAAAA";
-		for(int i=0;i<s.length();i++)
-		{
+		for (int i = 0; i < s.length(); i++) {
 
-			boolean isFound=false;
-			for(int k=0;k<i;k++)
-			{
-				if(s.charAt(i)==s.charAt(k))
-				{
-					isFound=true;
-					break;
-				}
-			}
-			if(isFound)
-				continue;
-			int count=1;
-			for(int j=i+1;j<s.length();j++)
-			{
-				for(int n=j+1;n<s.length();n++)
-				{
-					if(s.charAt(i)==s.charAt(j))
-						count++;
-				}
-			}
-			if(count>0)
-				System.out.print(s.charAt(i)+""+count);
+		    int count = 1;
+		    int j;
+
+		    for (j = i + 1; j < s.length(); j++) {
+		        if (s.charAt(i) == s.charAt(j))
+		            count++;
+		        else
+		            break;
+		    }
+
+		    System.out.print(s.charAt(i) + "" + count);
+
+		    // Move i to last counted character
+		    i = j - 1;
 		}
 
 	}
