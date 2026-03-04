e3d2bb8378afa5dee0575a1ed9f7e838d5fa281a
dadhikary2560 <d.adhikary2560@gmail.com>
2026-03-04 13:15:49 +0530

    adding new features, encapsulation to methods

--- src/encapsulationProgram/EncapsulationTest.java
+++ src/encapsulationProgram/EncapsulationTest.java
@@ -1,8 +1,17 @@
 package encapsulationProgram;
+
 public class EncapsulationTest {
-public static void main(String[] args) {
-	EncapsulationPrivate obj=new EncapsulationPrivate();
-	obj.setMarks(89);
-	System.out.println(obj.getMarks());
-}
+	public static void main(String[] args) {
+		EncapsulationPrivate obj = new EncapsulationPrivate();
+		obj.setMarks(89);
+		System.out.println(obj.getMarks());
+		
+		
+		System.out.println("---------------------------------------------------------");
+		
+		EncapsulationPrivateMethod obj2 = new EncapsulationPrivateMethod();
+		obj2.setA(20);
+		obj2.setB(30);
+		System.out.println(obj2.get());
+	}
 }
