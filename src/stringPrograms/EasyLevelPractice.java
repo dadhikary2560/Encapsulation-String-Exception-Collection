396d6de7c4b1ad3804f57208da979f841c3d4178
dadhikary2560 <d.adhikary2560@gmail.com>
2026-03-06 21:43:39 +0530

    practicing easy level string programs + assignments for interview

--- src/stringPrograms/EasyLevelPractice.java
+++ src/stringPrograms/EasyLevelPractice.java
@@ -1,6 +1,7 @@
 package stringPrograms;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class EasyLevelPractice {
 
@@ -37,6 +38,7 @@ public class EasyLevelPractice {
 			} else
 				System.out.println("special characters are: " + s.charAt(i));
 		}
+		System.out.println("no of total digits available: "+digit);
 		System.out.println("no of vowels available " + vowel);
 		System.out.println("no of consonants availbale " + consonanat);
 	}
@@ -101,40 +103,109 @@ public class EasyLevelPractice {
 		}
 		System.out.println(res);
 	}
-	
+
 	/*
-	 * 1) Reverse a string "automation" 
-	 * 2) Reverse a string array : I/p: " java selenium" O/P: "Selenium java"
-	 */	
-	
+	 * 1) Reverse a string "automation" 2) Reverse a string array : I/p:
+	 * " java selenium" O/P: "Selenium java"
+	 */
+
 	private static void m6() {
-		String s="automation";
-		String rev="";
-		
-		for(int i=s.length()-1;i>=0;i--)
-		{
-			rev=rev+s.charAt(i);
+		String s = "automation";
+		String rev = "";
+
+		for (int i = s.length() - 1; i >= 0; i--) {
+			rev = rev + s.charAt(i);
 		}
 		System.out.println(rev);
-		
-		String a="java selenium";
-		String arr[]=a.split(" ");
-		String reverse="";
-		for(int i=arr.length-1;i>=0;i--)
-		{
-			reverse=reverse+arr[i];
-			reverse=reverse+" ";
+
+		// second part
+
+		String a = "java selenium";
+		String arr[] = a.split(" ");
+		String reverse = "";
+		for (int i = arr.length - 1; i >= 0; i--) {
+			reverse = reverse + arr[i];
+			reverse = reverse + " ";
 		}
 		System.out.println(reverse);
 	}
 
+	private static void m7(Scanner obj) {
+		System.out.println("enter choice 1. for palindrome" + 
+	"2. to make character uppercase or lowercase"
+	+ "3. each odd word capitalise 4.occurrence of a specific character");
+		int choice = obj.nextInt();
+
+		// Check whether a string is palindrome.
+		if (choice == 1) {
+			String s = "malayalam";
+			String rev = "";
+			for (int i = s.length() - 1; i >= 0; i--) {
+				rev = rev + s.charAt(i);
+				System.out.println(rev);
+			}
+			if (rev.equals(s))
+				System.out.println("original string is palindrome: " + s);
+		}
+
+		// 5 Convert lowercase characters to uppercase and vice versa.
+		if (choice == 2) {
+			String s = "Hello java, this is string program";
+			s = s.toLowerCase();
+			String res = "";
+			for (int i = 0; i < s.length(); i++) {
+				if (i % 2 != 0)
+					res = res + Character.toUpperCase(s.charAt(i));
+				else
+					res = res + s.charAt(i);
+			}
+			System.out.println(res);
+		}
+		
+		//each odd word capitalize
+		if (choice == 3) {
+			String s = "Hello java, this is string program";
+			String arr[] = s.split(" ");
+			String res = "";
+			for (int i = 0; i < arr.length; i++) {
+				if (i % 2 != 0)
+					res = res + arr[i].toUpperCase();
+				else
+					res = res + arr[i];
+				res = res + " ";
+			}
+			System.out.println(res);
+		}
+		
+		
+		//Count occurrence of a specific character in a string.
+		if(choice==4)
+		{
+			String s = "Hello java, this is string program";
+			s=s.toLowerCase();
+			System.out.println("please enter the character you want to search for");
+			char c=obj.next().charAt(0);
+			int count=0;
+			for(int i=0;i<s.length();i++)
+			{
+				if(s.charAt(i)==c)
+					count++;
+			}
+			System.out.println("the count for "+c+" is: "+count);
+		}
+	}
+
 	public static void main(String[] args) {
 		EasyLevelPractice obj = new EasyLevelPractice();
-		// obj.m1(); //count characters without whitespace
-		// m2(); //count consonant | vowel | digits | special characters
-		// m3(); //reverse a string
-		// m4(); //capitalize the first letter from each word
-		//m5(); // Remove 1st letter from each word and capital the second word
-		m6();
+		Scanner sc = new Scanner(System.in);
+		obj.m1(); //count characters without whitespace
+		 m2(); //count consonant | vowel | digits | special characters
+		 m3(); //reverse a string
+		 m4(); //capitalize the first letter from each word
+		 m5(); // Remove 1st letter from each word and capital the second word
+		 m6(); //reverse a string and reverse one word at a time
+		 m7(sc); // palindrome string + toUpperCase/toLowerCase
+		
+		sc.close();
 	}
 }
